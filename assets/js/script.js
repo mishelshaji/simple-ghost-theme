b12e6f5c0b38b49316bac01ea9d15299d4512566
Mishel <mishelvettukattil@outlook.com>
2020-03-29 10:21:50 +0530

    Added search option

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,21 @@ if (typeof geekContentAPIKey !== 'undefined' && typeof geekSearchURL !== 'undefi
         template:function(result) {
             let url = [location.protocol, '//', location.host].join('');
             return '<li><a href="' + url + '/' + result.slug + '">' + result.title + '</a></li>';  
-        }
+        },
+        options: {
+            keys: [
+                'title',
+                'plaintext'
+            ],
+            limit: 15,
+            allowTypo: false
+        },
+        api: {
+            resource: 'posts',
+            parameters: { 
+                fields: ['title', 'slug', 'plaintext'],
+                formats: 'plaintext',
+            },
+        },
     });
 }
